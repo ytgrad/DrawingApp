ff29f999abb29d797a7cf627228810ea8636ddf2
ytgrad <ytgrad17@gmail.com>
2024-04-04 21:04:40 +0530

    permissions

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.drawingapp
 
+import android.Manifest
 import android.app.Dialog
+import android.content.pm.PackageManager
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,6 +10,7 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.view.get
 import com.example.drawingapp.databinding.ActivityMainBinding
 import com.example.drawingapp.databinding.DialogBrushSizeBinding
@@ -23,9 +26,41 @@ class MainActivity : AppCompatActivity() {
         binding.ibBrush.setOnClickListener {
             showBrushSizeDialog()
         }
+
+        binding.ibGallery.setOnClickListener {
+            requestPermission()
+        }
+
     }
 
+    private fun requestPermission(){
+        val permissionsToRequest = mutableListOf<String>()
+        if(!hasReadImagesPermission()){
+            permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }else{
+            Toast.makeText(this, "have access", Toast.LENGTH_SHORT).show()
+        }
+        if(permissionsToRequest.isNotEmpty()){
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), 0)
+        }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 0 && grantResults.isNotEmpty()){
+            for(i in grantResults.indices){
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    Log.d("permissions request: ", "${permissions[i]} was granted")
+                }
+            }
+        }
+    }
+    private fun hasReadImagesPermission() =
+        ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
 
     private fun showBrushSizeDialog(){
         val brushDialog = Dialog(this)
@@ -46,7 +81,6 @@ class MainActivity : AppCompatActivity() {
 
         brushDialog.show()
     }
-
     fun onPalletClick(view: View){
         val tempColor = (view as ImageButton).tag
 
